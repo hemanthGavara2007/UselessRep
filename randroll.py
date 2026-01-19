6d8f632ed6e51ae1ccf68b0def667803d268fbf3
hemanthGavara2007 <hemanthgavara9@gmail.com>
2026-01-19 10:40:57 +0530

    Update randroll.py
    - Added Changes if remaining no of roll no is less than no of members in a team
    - Added Input option for three variables

--- randroll.py
+++ randroll.py
@@ -1,12 +1,15 @@
 #given 60 roll no , divide each into 5 groups by random ways
 import random
+m = int(input("Enter starting roll no: "))
+n = int(input("Enter ending roll no: "))
+i = int(input("Enter no of persons in each group:  "))
 
-roll = [x for x in  range(501,561)]
+roll = [x for x in  range(m,n+1)]
 teams = []
 
 while roll:
     team = []
-    while len(team) < 5:
+    while len(team) < i and len(roll) > 0:
         x = random.randint(0,len(roll)-1)
         team.append(roll.pop(x))
 
